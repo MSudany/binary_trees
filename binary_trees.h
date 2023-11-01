762b9ff3f432757492a122273ace2c34b470088e
msudany <mssodany2002@gmail.com>
2023-11-01 19:51:05 +0200

    0. New node

--- binary_trees.h
+++ binary_trees.h
@@ -1,5 +1,8 @@
-#ifndef binary_trees.h
-#define binary_trees.h
+#ifndef binary_trees_h
+#define binary_trees_h
+
+#include <stdlib.h>
+#include <stdio.h>
 
 /**
  * struct binary_tree_s - Binary tree node
@@ -17,10 +20,10 @@ struct binary_tree_s
 	struct binary_tree_s *right;
 };
 
-void binary_tree_print(const binary_tree_t *);
 typedef struct binary_tree_s binary_tree_t;
 typedef struct binary_tree_s avl_t;
 typedef struct binary_tree_s heap_t;
+void binary_tree_print(const binary_tree_t *);
 
 /* tasks */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
