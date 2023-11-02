182168e35ffc51f6bec2b1a80275eeacd71bf7ea
msudany <mssodany2002@gmail.com>
2023-11-02 21:44:35 +0200

    8. Post-order

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -5,15 +5,13 @@
  * @tree: ...
  * @func: ...
  *
- * Return: void
  */
-
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if (tree && func)
 	{
 		binary_tree_postorder(tree->left, func);
-		binary_tree_postorder(tree->left, func);
-		func(tree->n);		
+		binary_tree_postorder(tree->right, func);
+		func(tree->n);
 	}
 }
